231fae37d24a974680180a8c6df96a797051ea69
Jesse <JesseAbarca888@gmail.com>
2022-08-20 00:33:18 -0700

    Create elements, first modal is complete

--- TaskTrackerReDo/js/script.js
+++ TaskTrackerReDo/js/script.js
@@ -1,253 +1,271 @@
 //UPDATE: 8/11/22, the form inside the modal body is done
 
-
 //My Js is connected!
 console.log("Hello World");
 
 //injection locations
-let todoColumn = document.getElementById('inject-to-do');
-let inProgressColumn = document.getElementById('inject-in-progress');
-let completedColumn = document.getElementById('inject-completed');
-
+let todoColumn = document.getElementById("inject-to-do");
+let inProgressColumn = document.getElementById("inject-in-progress");
+let completedColumn = document.getElementById("inject-completed");
 
 //btns
-let addTaskBtn = document.getElementById('add-task-btn');
-
-
-
-
+let addTaskBtn = document.getElementById("add-task-btn");
 
 const createBlock = (injectionLocation, cardType, taskTitle, taskDescription, taskPriority, dueDate) => {
-  //Modal 1
-  //Modal body
-  let modalBody = document.createElement('div');
-  let modalBodyForm = document.createElement('form');
+    //MODAL 1
 
-  //styling for the modal body
-  modalBody.className = 'modal-body';
+    //Modal 1 outermost div
+    let modal1OutermostDiv = document.createElement('div');
 
-  //row 1 vars and declarations
-  let modalBodyRow1 = document.createElement('div');
-  let modalBodyCol1 = document.createElement('div');
-  let modalLabelRow1 = document.createElement('label');
-  let modalInputRow1 = document.createElement('input');
+    //Modal 1 subdiv 1 from outermost div
+    let modal1SubDiv1 = document.createElement('div');
 
-  //row 2 vars and declarations
-  let modalBodyRow2 = document.createElement('div');
-  let modalBodyLabel2 = document.createElement('label');
-  let modalBodyCol2 = document.createElement('div');
-  let modalInputRow2 = document.createElement('input');
+    //Modal 1 subdiv 2 from outermost div
+    let modal1SubDiv2 = document.createElement('div');
 
-  //row 3 vars and declarations
-  let modalBodyRow3 = document.createElement('div');
-  let modalBodyLabel3 = document.createElement('label');
-  let modalBodyCol3 = document.createElement('div');
-  let modalBodySelect3 = document.createElement('select');
+    //Modal body
+    let modalBody = document.createElement('div');
+    let modalBodyForm = document.createElement('form');
+
+    //styling for modal 1 outermost div
+    modal1OutermostDiv.className = 'modal fade';
+    modal1OutermostDiv.id = 'exampleModal';
+    modal1OutermostDiv.setAttribute('tabindex', '-1');
+    modal1OutermostDiv.setAttribute('aria-labelledby', 'exampleModalLabel');
+    modal1OutermostDiv.setAttribute('aria-hidden', 'true');
+
+    //styling for subDiv1 from outermost div
+    modal1SubDiv1.className = 'modal-dialog';
+
+    //styling for subDiv2 from outermost div
+    modal1SubDiv2.className = 'modal-content';
+
+    //styling for the modal body
+    modalBody.className = "modal-body";
+
+    //row 1 vars and declarations
+    let modalBodyRow1 = document.createElement("div");
+    let modalBodyCol1 = document.createElement("div");
+    let modalLabelRow1 = document.createElement("label");
+    let modalInputRow1 = document.createElement("input");
+
+    //row 2 vars and declarations
+    let modalBodyRow2 = document.createElement("div");
+    let modalBodyLabel2 = document.createElement("label");
+    let modalBodyCol2 = document.createElement("div");
+    let modalInputRow2 = document.createElement("input");
+
+    //row 3 vars and declarations
+    let modalBodyRow3 = document.createElement("div");
+    let modalBodyLabel3 = document.createElement("label");
+    let modalBodyCol3 = document.createElement("div");
+    let modalBodySelect3 = document.createElement("select");
     //different options for the select
-  let modalBodySelectThreeOption1 = document.createElement('option');
-  let modalBodySelectThreeOption2 = document.createElement('option');
-  let modalBodySelectThreeOption3 = document.createElement('option');
-  let modalBodySelectThreeOption4 = document.createElement('option');
+    let modalBodySelectThreeOption1 = document.createElement("option");
+    let modalBodySelectThreeOption2 = document.createElement("option");
+    let modalBodySelectThreeOption3 = document.createElement("option");
+    let modalBodySelectThreeOption4 = document.createElement("option");
 
-  //row 3 vars and declarations
-  let modalBodyRow4 = document.createElement('div');
-  let modalBodyLabel4 = document.createElement('label');
-  let modalBodyCol4 = document.createElement('div');
-  let modalBodyInputRow4 = document.createElement('input');
+    //row 3 vars and declarations
+    let modalBodyRow4 = document.createElement("div");
+    let modalBodyLabel4 = document.createElement("label");
+    let modalBodyCol4 = document.createElement("div");
+    let modalBodyInputRow4 = document.createElement("input");
 
-  //Modal1 Header
-  let modal1Header = document.createElement('div');
-  let modal1HeaderTitle = document.createElement('h5');
-  let modal1HeaderCloseBtn = document.createElement('button');
+    //Modal1 Header
+    let modal1Header = document.createElement("div");
+    let modal1HeaderTitle = document.createElement("h5");
+    let modal1HeaderCloseBtn = document.createElement("button");
 
-  //Modal1 footer
-  let modal1Footer = document.createElement('div');
-  let modal1FooterCloseBtn1 = document.createElement('button');
-  let modal1FooterCloseBtn2 = document.createElement('button');
+    //Modal1 footer
+    let modal1Footer = document.createElement("div");
+    let modal1FooterCloseBtn1 = document.createElement("button");
+    let modal1FooterCloseBtn2 = document.createElement("button");
 
-  //styling for the first row
-  modalBodyRow1.className = 'mb-3 row';
-  modalBodyCol1.className = 'col-sm-9';
-  modalLabelRow1.className = 'col-sm-3 col-form-label';
-  modalLabelRow1.innerHTML = 'Name:';
-  modalLabelRow1.setAttribute('for', `${taskTitle}`);
-  modalInputRow1.className = 'form-control';
-  modalInputRow1.id = `${taskTitle}`;
-  modalInputRow1.setAttribute('type', 'text');
+    //styling for the first row
+    modalBodyRow1.className = "mb-3 row";
+    modalBodyCol1.className = "col-sm-9";
+    modalLabelRow1.className = "col-sm-3 col-form-label";
+    modalLabelRow1.innerHTML = "Name:";
+    modalLabelRow1.setAttribute("for", `${taskTitle}`);
+    modalInputRow1.className = "form-control";
+    modalInputRow1.id = `${taskTitle}`;
+    modalInputRow1.setAttribute("type", "text");
 
-  //styling for the second row
-  modalBodyRow2.className = 'mb-3 row';
-  modalBodyLabel2.className = 'col-sm-3 col-form-label';
-  modalBodyLabel2.innerHTML = 'Description:';
-  modalBodyLabel2.setAttribute('for', `${taskDescription}`);
-  modalBodyCol2.className = 'col-sm-9';
-  modalInputRow2.className = 'form-control';
-  modalInputRow2.id = `${taskDescription}`;
-  modalInputRow2.setAttribute('type', 'text');
+    //styling for the second row
+    modalBodyRow2.className = "mb-3 row";
+    modalBodyLabel2.className = "col-sm-3 col-form-label";
+    modalBodyLabel2.innerHTML = "Description:";
+    modalBodyLabel2.setAttribute("for", `${taskDescription}`);
+    modalBodyCol2.className = "col-sm-9";
+    modalInputRow2.className = "form-control";
+    modalInputRow2.id = `${taskDescription}`;
+    modalInputRow2.setAttribute("type", "text");
 
-  //styling for the third row
-  modalBodyRow3.className = 'mb-3 row';
-  modalBodyLabel3.className = 'col-sm-3 col-form-label';
-  modalBodyLabel3.innerHTML = 'Priority:';
-  modalBodyLabel3.setAttribute('for', `${taskPriority}`);
-  modalBodyCol3.className = 'col-sm-9';
-  modalBodySelect3.className = 'form-select';
-  modalBodySelect3.id = `${taskPriority}`;
-  modalBodySelect3.setAttribute('aria-label', 'Select a task priority');
-  modalBodySelectThreeOption1.setAttribute('selected', 'true');
-  modalBodySelectThreeOption1.innerHTML = 'Open this select menu';
-  modalBodySelectThreeOption2.setAttribute('value', '1');
-  modalBodySelectThreeOption2.innerHTML = 'To-Do';
-  modalBodySelectThreeOption3.setAttribute('value', '2');
-  modalBodySelectThreeOption3.innerHTML = 'In Progress';
-  modalBodySelectThreeOption4.setAttribute('value', '3');
-  modalBodySelectThreeOption4.innerHTML = 'High';
+    //styling for the third row
+    modalBodyRow3.className = "mb-3 row";
+    modalBodyLabel3.className = "col-sm-3 col-form-label";
+    modalBodyLabel3.innerHTML = "Priority:";
+    modalBodyLabel3.setAttribute("for", `${taskPriority}`);
+    modalBodyCol3.className = "col-sm-9";
+    modalBodySelect3.className = "form-select";
+    modalBodySelect3.id = `${taskPriority}`;
+    modalBodySelect3.setAttribute("aria-label", "Select a task priority");
+    modalBodySelectThreeOption1.setAttribute("selected", "true");
+    modalBodySelectThreeOption1.innerHTML = "Open this select menu";
+    modalBodySelectThreeOption2.setAttribute("value", "1");
+    modalBodySelectThreeOption2.innerHTML = "To-Do";
+    modalBodySelectThreeOption3.setAttribute("value", "2");
+    modalBodySelectThreeOption3.innerHTML = "In Progress";
+    modalBodySelectThreeOption4.setAttribute("value", "3");
+    modalBodySelectThreeOption4.innerHTML = "High";
 
-  //styling for the fourth row
-  modalBodyRow4.className = 'mb-2 row';
-  modalBodyLabel4.className = 'col-sm-3 col-form-label';
-  modalBodyLabel4.innerHTML = 'Due Date:';
-  modalBodyLabel4.setAttribute('for', `${dueDate}`);
-  modalBodyCol4.className = 'col-sm-9';
-  modalBodyInputRow4.className = 'calendarInput';
-  modalBodyInputRow4.id = 'dueDate';
-  modalBodyInputRow4.setAttribute('type', 'date');
+    //styling for the fourth row
+    modalBodyRow4.className = "mb-2 row";
+    modalBodyLabel4.className = "col-sm-3 col-form-label";
+    modalBodyLabel4.innerHTML = "Due Date:";
+    modalBodyLabel4.setAttribute("for", `${dueDate}`);
+    modalBodyCol4.className = "col-sm-9";
+    modalBodyInputRow4.className = "calendarInput";
+    modalBodyInputRow4.id = "dueDate";
+    modalBodyInputRow4.setAttribute("type", "date");
 
-//styling for the modal 1 header
-modal1Header.className = 'modal-header';
-modal1HeaderTitle.className = 'modal-title';
-modal1HeaderTitle.id = 'exampleModalLabel';
-modal1HeaderTitle.innerHTML = 'Add Task';
-modal1HeaderCloseBtn.className = 'btn-close';
-modal1HeaderCloseBtn.setAttribute('type', 'button');
-modal1HeaderCloseBtn.setAttribute('data-bs-dismiss', 'modal');
-modal1HeaderCloseBtn.setAttribute('aria-label', 'Close');
+    //styling for the modal 1 header
+    modal1Header.className = "modal-header";
+    modal1HeaderTitle.className = "modal-title";
+    modal1HeaderTitle.id = "exampleModalLabel";
+    modal1HeaderTitle.innerHTML = "Add Task";
+    modal1HeaderCloseBtn.className = "btn-close";
+    modal1HeaderCloseBtn.setAttribute("type", "button");
+    modal1HeaderCloseBtn.setAttribute("data-bs-dismiss", "modal");
+    modal1HeaderCloseBtn.setAttribute("aria-label", "Close");
 
-//styling for the modal 1 footer
-  modal1Footer.className = 'modal-footer';
-  modal1FooterCloseBtn1.className = 'btn btn-secondary';
-  modal1FooterCloseBtn1.setAttribute('data-bs-dismiss', 'modal');
-  modal1FooterCloseBtn1.setAttribute('type', 'button');
-  modal1FooterCloseBtn1.innerHTML = 'Close';
-  modal1FooterCloseBtn2.className = 'btn btn-primary';
-  modal1FooterCloseBtn2.setAttribute('type', 'button');
-  modal1FooterCloseBtn2.innerHTML = 'Save Changes';
+    //styling for the modal 1 footer
+    modal1Footer.className = "modal-footer";
+    modal1FooterCloseBtn1.className = "btn btn-secondary";
+    modal1FooterCloseBtn1.setAttribute("data-bs-dismiss", "modal");
+    modal1FooterCloseBtn1.setAttribute("type", "button");
+    modal1FooterCloseBtn1.innerHTML = "Close";
+    modal1FooterCloseBtn2.className = "btn btn-primary";
+    modal1FooterCloseBtn2.setAttribute("type", "button");
+    modal1FooterCloseBtn2.innerHTML = "Save Changes";
 
-  //putting the first modal row together
-  modalBodyRow1.appendChild(modalLabelRow1);
-  modalBodyRow1.appendChild(modalBodyCol1);
-  modalBodyCol1.appendChild(modalInputRow1);
+    //putting the first modal row together
+    modalBodyRow1.appendChild(modalLabelRow1);
+    modalBodyRow1.appendChild(modalBodyCol1);
+    modalBodyCol1.appendChild(modalInputRow1);
 
-  //putting the second modal row together
-  modalBodyRow2.appendChild(modalBodyLabel2);
-  modalBodyRow2.appendChild(modalBodyCol2);
-  modalBodyCol2.appendChild(modalInputRow2);
+    //putting the second modal row together
+    modalBodyRow2.appendChild(modalBodyLabel2);
+    modalBodyRow2.appendChild(modalBodyCol2);
+    modalBodyCol2.appendChild(modalInputRow2);
 
-  //putting the third modal row together
-  modalBodyRow3.appendChild(modalBodyLabel3);
-  modalBodyRow3.appendChild(modalBodyCol3);
-  modalBodyCol3.appendChild(modalBodySelect3);
-  modalBodySelect3.appendChild(modalBodySelectThreeOption1);
-  modalBodySelect3.appendChild(modalBodySelectThreeOption2);
-  modalBodySelect3.appendChild(modalBodySelectThreeOption3);
-  modalBodySelect3.appendChild(modalBodySelectThreeOption4);
+    //putting the third modal row together
+    modalBodyRow3.appendChild(modalBodyLabel3);
+    modalBodyRow3.appendChild(modalBodyCol3);
+    modalBodyCol3.appendChild(modalBodySelect3);
+    modalBodySelect3.appendChild(modalBodySelectThreeOption1);
+    modalBodySelect3.appendChild(modalBodySelectThreeOption2);
+    modalBodySelect3.appendChild(modalBodySelectThreeOption3);
+    modalBodySelect3.appendChild(modalBodySelectThreeOption4);
 
-  //putting the fourth modal row together
-  modalBodyRow4.appendChild(modalBodyLabel4);
-  modalBodyRow4.appendChild(modalBodyCol4);
-  modalBodyCol4.appendChild(modalBodyInputRow4);
+    //putting the fourth modal row together
+    modalBodyRow4.appendChild(modalBodyLabel4);
+    modalBodyRow4.appendChild(modalBodyCol4);
+    modalBodyCol4.appendChild(modalBodyInputRow4);
 
-  //putting the modal header together
-  modal1Header.appendChild(modal1HeaderTitle);
-  modal1Header.appendChild(modal1HeaderCloseBtn);
+    //putting the modal header together
+    modal1Header.appendChild(modal1HeaderTitle);
+    modal1Header.appendChild(modal1HeaderCloseBtn);
 
-  //putting the modal footer together
-  modal1Footer.appendChild('modal1FooterCloseBtn1');
-  modal1Footer.appendChild('modal1FooterCloseBtn2');
-  
-  //Putting the modal BODY TOGETHER
-  modalBody.appendChild(modalBodyForm);
-  modalBodyForm.appendChild(modalBodyRow1);
-  modalBodyForm.appendChild(modalBodyRow2);
-  modalBodyForm.appendChild(modalBodyRow3);
-  modalBodyForm.appendChild(modalBodyRow4);
+    //putting the modal footer together
+    modal1Footer.appendChild(modal1FooterCloseBtn1);
+    modal1Footer.appendChild(modal1FooterCloseBtn2);
 
-  //Putting the modal Together
-  //need body, header, footer
+    //Putting the modal BODY TOGETHER
+    modalBody.appendChild(modalBodyForm);
+    modalBodyForm.appendChild(modalBodyRow1);
+    modalBodyForm.appendChild(modalBodyRow2);
+    modalBodyForm.appendChild(modalBodyRow3);
+    modalBodyForm.appendChild(modalBodyRow4);
 
+    //Putting the modal Together
+    //need body, header, footer
+    modal1OutermostDiv.appendChild(modal1SubDiv1);
+    modal1SubDiv1.appendChild(modal1SubDiv2);
+    modal1SubDiv2.appendChild(modal1Header);
+    modal1SubDiv2.appendChild(modalBody);
+    modal1SubDiv2.appendChild(modal1Footer);
 
-  //----------------------------------------------------------------------------------------------------------------
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //three rows
-  let outermostDiv = document.createElement('div');
+    //----------------------------------------------------------------------------------------------------------------
 
-  //first row vars and declarations
-  let row1 = document.createElement('div');
-  let row1Div1 = document.createElement('div');
-  let row1h2 = document.createElement('h2');
+    //CREATE ELEMENTS FOR THE CARDS
+    let outermostDiv = document.createElement("div");
 
-  //second row vars and declarations
-  let row2 = document.createElement('div');
-  let row2Div2 = document.createElement('div');
-  let row2DivBtn = document.createElement('button');
+    //first row vars and declarations
+    let row1 = document.createElement("div");
+    let row1Div1 = document.createElement("div");
+    let row1h2 = document.createElement("h2");
 
-  //third row vars and declarations
-  let row3 = document.createElement('div');
-  let row3Div3 = document.createElement('div');
-  let row3DivBtn2 = document.createElement('button');
+    //second row vars and declarations
+    let row2 = document.createElement("div");
+    let row2Div2 = document.createElement("div");
+    let row2DivBtn = document.createElement("button");
 
-  //styling for first row
-  outermostDiv.className = `col-12 ${cardType} mb-3`;
-  row1.className = 'row';
-  row1Div1.className = 'col-12 mt-2';
-  row1h2.className = 'taskTitle text-center';
-  row1h2.innerHTML = taskTitle;
+    //third row vars and declarations
+    let row3 = document.createElement("div");
+    let row3Div3 = document.createElement("div");
+    let row3DivBtn2 = document.createElement("button");
 
-  //styling for the second row
-  row2.className = 'row mt-4';
-  row2Div2.className = 'col-12 d-flex justify-content-center';
-  row2DivBtn.className = 'btn btn-primary cardBtns';
-  row2DivBtn.innerHTML = 'Task Options';
+    //styling for first row
+    outermostDiv.className = `col-12 ${cardType} mb-3`;
+    row1.className = "row";
+    row1Div1.className = "col-12 mt-2";
+    row1h2.className = "taskTitle text-center";
+    row1h2.innerHTML = taskTitle;
 
-  //styling for the third row
-  row3.className = 'row mt-2 pb-3';
-  row3Div3.className = 'col-12 d-flex justify-content-center';
-  row3DivBtn2.className = 'btn btn-dark cardBtns';
-  row3DivBtn2.innerHTML = 'View Task';
+    //styling for the second row
+    row2.className = "row mt-4";
+    row2Div2.className = "col-12 d-flex justify-content-center";
+    row2DivBtn.className = "btn btn-primary cardBtns";
+    row2DivBtn.innerHTML = "Task Options";
 
-  //append everything together
-  outermostDiv.appendChild(row1);
-  outermostDiv.appendChild(row2);
-  outermostDiv.appendChild(row3);
+    //styling for the third row
+    row3.className = "row mt-2 pb-3";
+    row3Div3.className = "col-12 d-flex justify-content-center";
+    row3DivBtn2.className = "btn btn-dark cardBtns";
+    row3DivBtn2.innerHTML = "View Task";
 
-  //row 1 appending
-  row1.appendChild(row1Div1);
-  row1Div1.appendChild(row1h2);
+    //append everything together
+    outermostDiv.appendChild(row1);
+    outermostDiv.appendChild(row2);
+    outermostDiv.appendChild(row3);
 
-  //row2 appending
-  row2.appendChild(row2Div2);
-  row2Div2.appendChild(row2DivBtn);
+    //row 1 appending
+    row1.appendChild(row1Div1);
+    row1Div1.appendChild(row1h2);
 
-  //row3 appending
-  row3.appendChild(row3Div3);
-  row3Div3.appendChild(row3DivBtn2);
+    //row2 appending
+    row2.appendChild(row2Div2);
+    row2Div2.appendChild(row2DivBtn);
 
-  injectionLocation.appendChild(outermostDiv);
-}
+    //row3 appending
+    row3.appendChild(row3Div3);
+    row3Div3.appendChild(row3DivBtn2);
 
+    injectionLocation.appendChild(outermostDiv);
+};
 
+createBlock(todoColumn, 'todoCard', 'Task Tracker Project', 'Work on the Task tracker', 'High', '10/10/21');
 
+// 
+
+// injectionLocation,
+//     cardType,
+//     taskTitle,
+//     taskDescription,
+//     taskPriority,
+//     dueDate
 //-------------------------------------------------------------------------------------
 //modal HTML BELOW
 /*
