8077304308681584ce8884731cc3f22248230087
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-10 22:42:02 -0700

    Format index.html, styles.css, and localStorage.js with prettier

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,11 +1,10 @@
 export const saveTaskToLocalStorage = (item) => {
-    // get the previous tasks, save them into a variable []
-    // save the new task to the variable
-    // set local storage to JSON.stringify(variable)
-    const previousTasks = JSON.parse(localStorage.getItem("tasks"));
-    console.log(previousTasks);
-    localStorage.setItem("tasks", JSON.stringify(item));
-    const tasks = localStorage.getItem("tasks");
-    console.log(tasks)
+  // get the previous tasks, save them into a variable []
+  // save the new task to the variable
+  // set local storage to JSON.stringify(variable)
+  const previousTasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log(previousTasks);
+  localStorage.setItem("tasks", JSON.stringify(item));
+  const tasks = localStorage.getItem("tasks");
+  console.log(tasks);
 };
-
