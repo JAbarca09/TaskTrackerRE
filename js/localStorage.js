b1504ccc1f1f3aad9dbdbcad5172a5d58f7343cc
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-10 22:39:06 -0700

    Start on SaveToLocalStorage functionality

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,3 +1,11 @@
-export function hi(){
-    console.log("js file 2");
-}
+export const saveTaskToLocalStorage = (item) => {
+    // get the previous tasks, save them into a variable []
+    // save the new task to the variable
+    // set local storage to JSON.stringify(variable)
+    const previousTasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(previousTasks);
+    localStorage.setItem("tasks", JSON.stringify(item));
+    const tasks = localStorage.getItem("tasks");
+    console.log(tasks)
+};
+
