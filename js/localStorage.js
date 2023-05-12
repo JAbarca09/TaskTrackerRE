ad9574261bf631828d6466a7a790833c366154e4
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-12 01:23:17 -0700

    Add saveTaskToLocalStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,10 +1,15 @@
-export const saveTaskToLocalStorage = (item) => {
+export const saveTaskToLocalStorage = (task) => {
   // get the previous tasks, save them into a variable []
   // save the new task to the variable
   // set local storage to JSON.stringify(variable)
-  const previousTasks = JSON.parse(localStorage.getItem("tasks"));
-  console.log(previousTasks);
-  localStorage.setItem("tasks", JSON.stringify(item));
-  const tasks = localStorage.getItem("tasks");
-  console.log(tasks);
+  const previousTasksArr = JSON.parse(localStorage.getItem("tasks"));
+  if (previousTasksArr !== null) {
+    //there is a task
+    previousTasksArr.push(task);
+    localStorage.setItem("tasks", JSON.stringify(previousTasksArr));
+  } else {
+    // create the arr with one task and stringify it!
+    let tempArr = [task];
+    localStorage.setItem("tasks", JSON.stringify(tempArr));
+  }
 };
