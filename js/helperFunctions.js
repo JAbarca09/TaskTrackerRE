262e4ba98a99bc36d9c0af0ad6b2ef2b5911f4da
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-06 21:03:04 -0700

    Add taskPriorityText function

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -1,17 +1,38 @@
 // Add functions needed for data validation here!
 
 const checkIfInputEmpty = (input) => {
-  const isValidInput = input.trim() !== "";
+  const isValidInput = input.trim() !== '';
   return isValidInput;
 };
 
 const checkIfPriorityIsValid = (input) => {
-  const isValidPriority = input !== "Open this select menu";
+  const isValidPriority = input !== 'Open this select menu';
   return isValidPriority;
 };
 
 const truncateTaskName = (str, max) => {
-  return str.length > max ? str.substr(0, max - 1) + "…" : str;
+  return str.length > max ? str.substr(0, max - 1) + '…' : str;
 };
 
-export { checkIfInputEmpty, checkIfPriorityIsValid, truncateTaskName };
+const taskPriorityText = (priority) => {
+  let taskPriorityTxt;
+  switch (priority) {
+    case '1':
+      taskPriorityTxt = 'To-do';
+      break;
+    case '2':
+      taskPriorityTxt = 'In Progress';
+      break;
+    case '3':
+      taskPriorityTxt = 'Complete';
+      break;
+  }
+  return taskPriorityTxt;
+};
+
+export {
+  checkIfInputEmpty,
+  checkIfPriorityIsValid,
+  truncateTaskName,
+  taskPriorityText,
+};
