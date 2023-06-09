afc890754b2ae223f99a3f1b794e03dc0387a06e
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-08 22:08:07 -0700

    Add format date function

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -30,9 +30,17 @@ const taskPriorityText = (priority) => {
   return taskPriorityTxt;
 };
 
+const formatDate = (date) => {
+  const year = date.slice(0, 4);
+  const month = date.slice(5, 7);
+  const day = date.slice(8, 10);
+  return `${month}/${day}/${year}`;
+};
+
 export {
   checkIfInputEmpty,
   checkIfPriorityIsValid,
   truncateTaskName,
   taskPriorityText,
+  formatDate,
 };
